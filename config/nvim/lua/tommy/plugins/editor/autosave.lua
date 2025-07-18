a3779d9071fcf3c9332325963923145b1121d1a5
Tommy Tran Duc Thang <tranthang.dev@gmail.com>
2025-07-18 17:21:49 +0700

    feat: update autosave to exclude oil

--- config/nvim/lua/tommy/plugins/editor/autosave.lua
+++ config/nvim/lua/tommy/plugins/editor/autosave.lua
@@ -1,6 +1,20 @@
 return {
   "Pocco81/auto-save.nvim",
   config = function()
-    require("auto-save").setup {}
+    require("auto-save").setup {
+      condition = function(buf)
+        local fn = vim.fn
+        local utils = require("auto-save.utils.data")
+
+        -- Exclude oil filetype
+        local excluded_filetypes = { "oil" }
+
+        if fn.getbufvar(buf, "&modifiable") == 1 and
+            utils.not_in(fn.getbufvar(buf, "&filetype"), excluded_filetypes) then
+          return true
+        end
+        return false
+      end
+    }
   end,
 }
