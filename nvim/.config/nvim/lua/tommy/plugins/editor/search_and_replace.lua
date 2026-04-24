9075af26e89419d002a9e8afde5663ce09060131
Tommy Tran Thang <tranthang.dev@gmail.com>
2026-04-24 11:56:36 +0700

    fix(nvim): search and replace dependancy

--- nvim/.config/nvim/lua/tommy/plugins/editor/search_and_replace.lua
+++ nvim/.config/nvim/lua/tommy/plugins/editor/search_and_replace.lua
@@ -2,7 +2,9 @@ return {
   "nvim-pack/nvim-spectre",
   commit = "72f56f7585903cd7bf92c665351aa585e150af0f",
   build = false,
-  cmd = "Spectre",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
   opts = { open_cmd = "noswapfile vnew" },
   -- stylua: ignore
   keys = {
